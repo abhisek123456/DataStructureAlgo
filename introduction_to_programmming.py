e05fcf166f79a248a4739596a9f42f9456aa2a42
Abhisek Datta <abhi.datta07@gmail.com>
2022-11-08 19:44:42 +0530

    Episode 1

--- introduction_to_programmming.py
+++ introduction_to_programmming.py
@@ -1,5 +1,26 @@
-print("Enter the final number upto which you want to print")
+""" print("Enter the final number upto which you want to print")
 a=int(input())
 i=0
 for i in range(a):
-    print(i)
+    print(i) """
+""" 
+print("Enter the number")
+a=int(input())
+i=0
+for i in range(a):
+    if i%2==0:
+        print("the even number is " , i) """
+""" 
+
+print("Enter the number")
+a=int(input())
+li=[]
+for i in range(1,a):
+    li.append(i)
+li.append(a)
+Sum=sum(li)
+print("The sum is ",Sum) """
+
+
+print("Enter the number")
+a=int(input())
